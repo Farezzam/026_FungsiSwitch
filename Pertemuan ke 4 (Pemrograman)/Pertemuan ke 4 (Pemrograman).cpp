4092276c953bf11357bb0711f8bea0600476cad3
ASUS <faris13abiyu@gmail.com>
2024-03-26 17:47:36 +0700

    tampilan pilihan

--- Pertemuan ke 4 (Pemrograman)/Pertemuan ke 4 (Pemrograman).cpp
+++ Pertemuan ke 4 (Pemrograman)/Pertemuan ke 4 (Pemrograman).cpp
@@ -11,3 +11,46 @@ void input()
 	cin >> bilB;
 }
 
+int penjumlahan(int a, int b)
+{
+	return a + b;
+}
+
+int pengurangan(int a, int b)
+{
+	return a - b;
+}
+
+int perkalian(int a, int b)
+{
+	return a * b;
+}
+
+int pembagiab(int a, int b)
+{
+	return a / b;
+}
+
+int main()
+{
+	int pilihan;
+	do
+	{
+		cout << "Kalkulator Sederhana" << endl;
+		cout << "=====================" << endl;
+		cout << "1. Penjumlahan" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Perkalian" << endl;
+		cout << "4. Pembagian" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "=====================" << endl;
+		cout << "Masukkan menu pilihan = " << endl;
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
+
+		}
+	}
+}
+
